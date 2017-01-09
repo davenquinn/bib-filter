8da6f3fba4a88c256272c29a33d1f934ae98b0e1
Daven Quinn <dev@davenquinn.com>
2017-01-09 02:20:30 -0800

    Added natbib/bibtex options

--- bib_filter.py
+++ bib_filter.py
@@ -2,21 +2,24 @@
 
 import click
 import bibtexparser
-from shutil import copy
 from os import path
 from re import compile
 
 file_ = click.File('r')
-pattern = compile(r'(?<=^\\citation\{)(.+)(?=\})')
+patterns = {
+    'natbib': r'(?<=^\\citation\{)(.+)(?=\})',
+    'biblatex': r'(?<=\\abx@aux@cite\{)(.+)(?=\})'
+}
 
 def parse_list(keyfile):
     return (k.strip() for k in keyfile.readlines())
 
-def parse_aux(auxfile):
+def parse_aux(auxfile, backend='natbib'):
     """
     Gets citations from aux file. Doesn't work with
     biblatex as yet.
     """
+    pattern = compile(patterns[backend])
     for l in auxfile.readlines():
         key = pattern.search(l)
         if key is None: continue
@@ -28,7 +31,9 @@ def parse_aux(auxfile):
 @click.option('--keys','-k', type=file_)
 @click.option('--aux','-a', type=file_)
 @click.option('--clean', is_flag=True, default=False)
-def cli(library,outfile,keys=None,aux=None, clean=False):
+@click.option('--natbib','backend',flag_value='natbib', default=True)
+@click.option('--biblatex', 'backend',flag_value='biblatex')
+def cli(library,outfile,keys=None,aux=None, clean=False, backend='natbib'):
 
     db = bibtexparser.load(library)
 
@@ -36,7 +41,7 @@ def cli(library,outfile,keys=None,aux=None, clean=False):
     if keys is not None:
         _keys += list(parse_list(keys))
     if aux is not None:
-        _keys += list(parse_aux(aux))
+        _keys += list(parse_aux(aux, backend=backend))
     _keys = set(_keys)
     click.echo(" ".join(_keys))
 
